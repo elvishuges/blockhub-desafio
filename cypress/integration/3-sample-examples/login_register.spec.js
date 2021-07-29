88daad98985f3bb7e4b52ab2259b6c2f8d0a7a09
elvishuges <elvishuges@hotmail.com>
2021-07-28 21:46:52 -0300

    🎨 update arquivos de projeto Bar e drawer

--- cypress/integration/3-sample-examples/login_register.spec.js
+++ cypress/integration/3-sample-examples/login_register.spec.js
@@ -1,4 +1,11 @@
-describe("direct to login and register page", () => {
+const registeredUser = {
+  email: "devopsuser@hotmail.com",
+  password: "123456",
+};
+
+const TokenKey = "blockhub.api.auth";
+
+describe("test initial page login, register and forguet password", () => {
   it("try to login with invalids fields", () => {
     cy.visit("/");
     cy.get("input[name=email]").type("elvishuges@");
@@ -49,10 +56,31 @@ describe("direct to login and register page", () => {
       .should("eq", 403);
   });
 
-  it("go back to login interface", function() {
+  it("go back to login interface and login and acess dashboard", function() {
     // programmatically log us in without needing the UI
-    cy.visit("/login");
-    cy.get("[data-cy=submit]").click();
-    cy.url().should("include", "/login");
+    cy.visit("/login"); // programmatically log us in without needing the UI
+    window.localStorage.removeItem(TokenKey);
+    cy.get("input[name=email]").type(registeredUser.email);
+    cy.get("input[name=password]").type(registeredUser.password);
+    //cy.get("[data-cy=submit]").click();
+    cy.request({
+      method: "post",
+      url: "https://api-desafio-blockub.herokuapp.com/login",
+      failOnStatusCode: false,
+      body: {
+        email: registeredUser.email,
+        password: registeredUser.password,
+      },
+    }).then((response) => {
+      // response.body is automatically serialized into JSON
+      console.log("response login", response);
+      window.localStorage.setItem(TokenKey, response.body.access_token);
+      expect(response.status).to.eq(201);
+      expect(response).to.have.property("headers");
+      cy.reload();
+      cy.url().should("include", "/dashboard");
+    });
   });
+
+  it("programmatic login user", function() {});
 });
